ea2cd88eb145bd8eac5ddab4156291f3b76ee4d1
dragondaddy <dragondaddy@caselibre.fr>
2023-10-23 21:38:09 +0200

    Cronjobs & daily update script rework

--- conf/daily-update.sh
+++ conf/daily-update.sh
@@ -6,7 +6,6 @@ echo " "
 echo "$(date) - db size."
 du -h /var/lib/mysql/__APP__
 #
-cd /var/www/__APP__
-/usr/bin/php__PHPVERSION__ util/udall
+(cd /var/www/__APP__ ; sudo -u streams util/udall)
 echo "$(date) - Daily update finished."
 
