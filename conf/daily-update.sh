e79d7bab15dd6e99b815dc7434eb8869f7da96ad
dragondaddy <dragondaddy@caselibre.fr>
2023-10-24 12:54:49 +0200

    Playing with users & permissions again

--- conf/daily-update.sh
+++ conf/daily-update.sh
@@ -3,9 +3,7 @@
 echo " "
 echo "+++ $(date) +++"
 echo " "
-echo "$(date) - db size."
-du -h /var/lib/mysql/__APP__
-#
+echo "$(date) - Updating website..."
 (cd /var/www/__APP__ ; sudo -u streams util/udall)
 echo "$(date) - Daily update finished."
 
